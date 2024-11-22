589e116d9d400b595e1f457853416665e51b0aad
sruthi <sruthika.n2004@gmail.com>
2024-11-22 09:05:15 +0530

    commit-1

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main { // The class name matches the file name
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Print a message to the console
+    }
+}
